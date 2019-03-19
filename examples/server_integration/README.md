20ed3b1c3e6c68fab22371d6204bbc4b7cc3b6fc
Klemens Schölhorn <klemens@schoelhorn.eu>
2019-03-19 21:17:52 +0100

    Serve statics through Rocket in the server integration example
    When using Rocket as an http server, it seems logical to also use it to
    serve the index page and the wasm/js package files. This allows us to get
    rid of rocket_cors and it simplifies the server code quite a bit.
    
    This commit also bumps rocket to 0.4.0 and imrpoves the README.

--- examples/server_integration/README.md
+++ examples/server_integration/README.md
@@ -1,8 +1,12 @@
 # Server integration
 
-A demonstration of sharing data structures between client and server when using
-a Rust backend. This reduces duplication. Also provides examples of a simple get request.
- Uses Rocket for the backend.
- 
- To run, from this directory, run `cargo +nightly run`, and from the `frontend`
- subdirectory, run the build script and dev server as you would normally.
+A demonstration of sharing data structures between client and server when
+using a Rust backend to avoid duplication and incompatibilities. It uses
+serde for data de/serialization and Rocket for the backend. It also contains
+a simple exmaple for a get request.
+
+## Execute
+
+Run the build script in the `frontend` directory to compile and package the
+frontend part and afterwards start the server by executing `cargo +nightly run`
+in this directory.
--- examples/server_integration/src/main.rs
+++ examples/server_integration/src/main.rs
@@ -1,18 +1,8 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] 
-extern crate rocket;
-use rocket::http::Method;
-use rocket_cors;
-use rocket_cors::{AllowedHeaders, AllowedOrigins};
-
+use rocket::{get, routes};
+use rocket_contrib::serve::StaticFiles;
 use shared::Data;
-
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
 
 #[get("/data", format = "application/json")]
 fn data_api() -> String {
@@ -25,20 +15,8 @@ fn data_api() -> String {
 }
 
 fn main() {
-    let (allowed_origins, failed_origins) = AllowedOrigins::some(&["http://localhost:8000"]);
-    assert!(failed_origins.is_empty());
-
-    // You can also deserialize this
-    let options = rocket_cors::Cors {
-        allowed_origins: allowed_origins,
-        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
-        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
-        allow_credentials: true,
-        ..Default::default()
-    };
-
     rocket::ignite()
-        .mount("/", routes![index, data_api])
-        .attach(options)
+        .mount("/", StaticFiles::from("./frontend"))
+        .mount("/", routes![data_api])
         .launch();
 }
