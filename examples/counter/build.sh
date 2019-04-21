4d5da4af7b7a04b025410be414c569e0489e7ab4
Martin Kavík <martin@kavik.cz>
2019-04-22 01:53:19 +0200

    Makefile.toml, .gitignore
    Makefile.tom

--- examples/counter/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cargo build --target wasm32-unknown-unknown
-wasm-bindgen target/wasm32-unknown-unknown/debug/counter.wasm --no-modules --out-dir ./pkg --out-name package
-
-
-#cargo build --target wasm32-unknown-unknown --release
-#wasm-bindgen target/wasm32-unknown-unknown/release/counter.wasm --no-modules --out-dir ./pkg --out-name package
