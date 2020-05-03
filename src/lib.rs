88dccc4c1892ae4dd4527b7c10f15808c1c6809f
Ben <benphawke@gmail.com>
2020-05-03 06:45:36 +0200

    Include Counter Example at home page of generated documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,50 @@
 //! Visit the [website](https://seed-rs.org/)
 //!
 //! See the [github Readme](https://github.com/seed-rs/seed) for details
+//!
+//!
+//! ## Counter Example
+//! ```
+//! use seed::{prelude::*, *};
+//!
+//! // `init` describes what should happen when your app started.
+//! fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
+//!     Model::default()
+//! }
+//!
+//! // `Model` describes our app state.
+//! type Model = i32;
+//!
+//! // `Msg` describes the different events you can modify state with.
+//! enum Msg {
+//!     Increment,
+//! }
+//!
+//! // `update` describes how to handle each `Msg`.
+//! fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+//!     match msg {
+//!         Msg::Increment => *model += 1,
+//!     }
+//! }
+//!
+//! // `view` describes what to display.
+//! fn view(model: &Model) -> Node<Msg> {
+//!     div![
+//!         "This is a counter: ",
+//!         C!["counter"],
+//!         button![
+//!             model,
+//!             ev(Ev::Click, |_| Msg::Increment),
+//!         ],
+//!     ]
+//! }
+//!
+//! #[wasm_bindgen(start)]
+//! pub fn start() {
+//!     // Mount the `app` to the element with the `id` "app".
+//!     App::start("app", init, update, view);
+//! }
+//! ```
 
 //#![deny(missing_docs)]
 #![forbid(unsafe_code)]
