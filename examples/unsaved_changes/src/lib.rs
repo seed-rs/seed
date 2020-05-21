b3c1e0748580fb70a09ea3856ae2aba23ad02514
Martin Kavík <martin@kavik.cz>
2020-05-21 11:34:30 +0200

    fix: correct url_request_handler priority

--- examples/unsaved_changes/src/lib.rs
+++ examples/unsaved_changes/src/lib.rs
@@ -9,6 +9,7 @@ const STORAGE_KEY: &str = "seed_unsaved_changes_text";
 //     Init
 // ------ ------
 
+#[allow(clippy::needless_pass_by_value)]
 fn init(url: Url, orders: &mut impl Orders<Msg>) -> Model {
     // https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onbeforeunload
     orders
@@ -39,10 +40,10 @@ struct Model {
 
 struct_urls!();
 impl<'a> Urls<'a> {
-    pub fn home(self) -> Url {
+    fn home(self) -> Url {
         self.base_url()
     }
-    pub fn no_home(self) -> Url {
+    fn no_home(self) -> Url {
         self.base_url().add_path_part("no-home")
     }
 }
@@ -64,11 +65,18 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
         Msg::Save => {
             LocalStorage::insert(STORAGE_KEY, &model.text).expect("save text");
             model.saved_text_hash = calculate_hash(&model.text);
-        },
+        }
         Msg::UrlRequested(subs::UrlRequested(_, url_request)) => {
-            log!("prevent!");
+            if calculate_hash(&model.text) == model.saved_text_hash {
+                return;
+            }
+            if let Ok(true) =
+                window().confirm_with_message("Do you want to leave? Data won't be saved.")
+            {
+                return;
+            }
             url_request.handled_and_prevent_refresh();
-        },
+        }
         Msg::BeforeUnload(event) => {
             if calculate_hash(&model.text) != model.saved_text_hash {
                 log!("attempt to prevent navigation");
@@ -93,21 +101,27 @@ fn calculate_hash<T: Hash>(value: &T) -> u64 {
 
 fn view(model: &Model) -> Node<Msg> {
     div![
-        style!{St::Display => "flex", St::FlexDirection => "column", St::MaxWidth => px(250), St::Margin => "auto", St::MarginTop => px(30)},
+        style! {St::Display => "flex", St::FlexDirection => "column", St::MaxWidth => px(250), St::Margin => "auto", St::MarginTop => px(30)},
         textarea![
-            attrs!{At::Value => model.text},
+            attrs! {At::Value => model.text},
             input_ev(Ev::Input, Msg::TextChanged),
         ],
         div![
-            style!{St::Display => "flex", St::JustifyContent => "space-between", St::Padding => px(10)},
+            style! {St::Display => "flex", St::JustifyContent => "space-between", St::Padding => px(10)},
             span![IF!(calculate_hash(&model.text) != model.saved_text_hash => "Unsaved changes")],
             button![ev(Ev::Click, |_| Msg::Save), "Save"],
         ],
         div![
-            style!{St::Display => "flex", St::JustifyContent => "space-between"},
-            a![attrs!{At::Href => Urls::new(&model.base_url).home()}, "Home"],
-            a![attrs!{At::Href => Urls::new(&model.base_url).no_home()}, "/no-home"],
-            a![attrs!{At::Href => "https://example.com"}, "example.com"],
+            style! {St::Display => "flex", St::JustifyContent => "space-between"},
+            a![
+                attrs! {At::Href => Urls::new(&model.base_url).home()},
+                "Home"
+            ],
+            a![
+                attrs! {At::Href => Urls::new(&model.base_url).no_home()},
+                "/no-home"
+            ],
+            a![attrs! {At::Href => "https://example.com"}, "example.com"],
         ]
     ]
 }
--- src/app/effects.rs
+++ src/app/effects.rs
@@ -1,24 +1,22 @@
 use super::{MessageMapper, Notification};
 
 pub enum Effect<Ms, GMs> {
-    Msg(Ms),
+    Msg(Option<Ms>),
     GMsg(GMs),
     Notification(Notification),
-}
-
-impl<Ms, GMs> From<Ms> for Effect<Ms, GMs> {
-    fn from(message: Ms) -> Self {
-        Effect::Msg(message)
-    }
+    TriggeredHandler(Box<dyn Fn() -> Option<Ms>>),
 }
 
 impl<Ms: 'static, OtherMs: 'static, GMs> MessageMapper<Ms, OtherMs> for Effect<Ms, GMs> {
     type SelfWithOtherMs = Effect<OtherMs, GMs>;
     fn map_msg(self, f: impl FnOnce(Ms) -> OtherMs + 'static + Clone) -> Effect<OtherMs, GMs> {
         match self {
-            Effect::Msg(msg) => Effect::Msg(f(msg)),
+            Effect::Msg(msg) => Effect::Msg(msg.map(f)),
             Effect::GMsg(g_msg) => Effect::GMsg(g_msg),
             Effect::Notification(notification) => Effect::Notification(notification),
+            Effect::TriggeredHandler(handler) => {
+                Effect::TriggeredHandler(Box::new(move || handler().map(f.clone())))
+            }
         }
     }
 }
