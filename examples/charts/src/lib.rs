1888ed4e130208bc59b2337398d18cc8000d665d
TatriX <tatrics@gmail.com>
2020-05-22 15:33:16 +0200

    examples: Add axis, ticks and tooltip to charts

--- examples/charts/src/lib.rs
+++ examples/charts/src/lib.rs
@@ -8,18 +8,22 @@ type Point = (f64, f64);
 
 struct Model {
     line_data: Vec<Point>,
+    tooltip: Option<(i32, i32)>,
 }
 
 impl Default for Model {
     fn default() -> Self {
         let end = 6.0 * std::f64::consts::PI;
         let n = 300;
-        Self{
-            line_data: (0..n).map(|i| {
-                let x = f64::from(i) * end / f64::from(n);
-                let y = f1(x);
-                (x, y)
-            }).collect()
+        Self {
+            line_data: (0..n)
+                .map(|i| {
+                    let x = f64::from(i) * end / f64::from(n);
+                    let y = f1(x);
+                    (x, y)
+                })
+                .collect(),
+            tooltip: None,
         }
     }
 }
@@ -28,14 +32,49 @@ fn f1(x: f64) -> f64 {
     x.sin()
 }
 
-enum Msg {}
+#[derive(Clone)]
+enum Msg {
+    ShowTooltip(i32, i32),
+    HideTooltip,
+}
 
-fn update(_msg: Msg, _model: &mut Model, _: &mut impl Orders<Msg>) {}
+fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::ShowTooltip(x, y) => {
+            model.tooltip = Some((x, y));
+        },
+        Msg::HideTooltip => {
+            model.tooltip = None;
+        }
+    }
+}
 
 fn view(model: &Model) -> Node<Msg> {
     div![
-        style!{St::Width => "80vh", St::Height => "50vh", St::Margin => "auto"},
-        line::chart(&model.line_data),
+        style! {St::Width => "80vh", St::Height => "50vh", St::Margin => "auto"},
+        div![
+            style! {
+                St::Position => "relative",
+                St::Border => "1px solid #ddd",
+            },
+            line::chart(&model.line_data, Msg::ShowTooltip, Msg::HideTooltip),
+            model
+                .tooltip
+                .map(|(x, y)| div![
+                    style! {
+                        St::PointerEvents => "none",
+                        St::Position => "fixed",
+                        St::Left => px(x),
+                        St::Top => px(y - 50),
+                        St::Background => "#eee",
+                        St::Border => "1px solid #ccc",
+                        St::BoxShadow => "2px 2px 4px rgba(0, 0, 0, 0.2)",
+                        St::Padding => px(10),
+                        St::BorderRadius => px(3),
+                    },
+                    format!("Tooltip {} {}", x, y),
+                ])
+        ]
     ]
 }
 
--- examples/charts/src/line.rs
+++ examples/charts/src/line.rs
@@ -1,51 +1,133 @@
-use seed::{prelude::*, *};
 use itertools::Itertools;
+use seed::{prelude::*, *};
 
-pub fn chart<T: 'static>(data: &[(f64, f64)]) -> Node<T> {
+pub fn chart<T: Clone + 'static>(
+    data: &[(f64, f64)],
+    onenter: impl Fn(i32, i32) -> T + Clone + 'static,
+    onout: T,
+) -> Node<T> {
+    use At::*;
     // domain
-    let x_bounds = data
-        .iter()
-        .map(|(x, _)| x)
-        .minmax()
-        .into_option()
-        .unwrap();
+    let x_bounds = data.iter().map(|(x, _)| x).minmax().into_option().unwrap();
 
-    let y_bounds = data
-        .iter()
-        .map(|(_, y)| y)
-        .minmax()
-        .into_option()
-        .unwrap();
+    // let y_bounds = data.iter().map(|(_, y)| y).minmax().into_option().unwrap();
 
-    let n = data.len() as f64;
-    let dx = (x_bounds.1 - x_bounds.0) / n; // step between points
+    let padding = 30;
+    let width = 800;
+    let height = 500;
 
+    // let n = data.len() as f64;
+    let n = width as f64;
+    let dx = (x_bounds.1 - x_bounds.0) / (n - (2 * padding) as f64); // scale factor
+    let ticks_num = 15;
+    let ticks_step = (
+        (width - 2 * padding) / ticks_num,
+        (height - 2 * padding) / ticks_num,
+    );
     svg![
-        style!{
-            St::Border => "1px solid #ccc",
-        },
-
+        class!["chart"],
+        style!{ St::Display => "block" },
         attrs! {
-            At::Width => "100%",
-            At::Height => "100%",
-            At::ViewBox => format!("{} {} {} {}",
-                                   x_bounds.0 / dx,
-                                   y_bounds.0 / dx,
-
-                                   (x_bounds.1 - x_bounds.0) / dx,
-                                   (y_bounds.1 - y_bounds.0) / dx,
-            ),
+            ViewBox => format!("0 0 {} {}", width, height),
         },
-        // rect![attrs! {At::Width => "100%", At::Height => "100%", At::Fill => "rgba(0, 0, 0, 0.1)"},],
-
-        polyline![attrs! {
-            At::Fill => "none",
-            At::Stroke => "rgb(0, 86, 91)",
-            At::Points => data
-                .iter()
-                .map(|(x, y)| format!("{},{}", x  / dx, y / dx))
-                .collect::<Vec<_>>()
-                .join(" ")
-        }],
+        g![
+            class!["x-axis"],
+            line_![attrs! {
+                X1 => padding,
+                X2 => width - padding,
+                Y1 => height/2,
+                Y2 => height/2,
+                Stroke => "#ccc",
+            }],
+            (ticks_step.0..(width - 2 * padding))
+                .step_by(ticks_step.0)
+                .map(|i| rect![attrs! {
+                    X => i + padding,
+                    Y => "50%",
+                    Width => 1,
+                    Height => 3,
+                    Fill => "#999",
+                }]),
+            (ticks_step.0..(width - 2 * padding))
+                .step_by(ticks_step.0)
+                .map(|i| text![
+                    style! {St::FontSize => px(10)},
+                    attrs! {
+                        Fill => "#999",
+                        TextAnchor => "middle",
+                        X => i + padding,
+                        Y => height/2 + 20,
+                    },
+                    format!("{:.2}", (i as f64)*dx),
+                ]),
+        ],
+        g![
+            class!["y-axis"],
+            line_![attrs! {
+                X1 => padding,
+                X2 => padding,
+                Y1 => padding,
+                Y2 => height - padding,
+                Stroke => "#ccc",
+            }],
+            (ticks_step.1..(height - 2 * padding))
+                .step_by(ticks_step.1)
+                .map(|i| rect![attrs! {
+                    X => padding - 3,
+                    Y => i + padding,
+                    Width => 3,
+                    Height => 1,
+                    Fill => "#999",
+                }],),
+            (ticks_step.1..(height - 2 * padding))
+                .step_by(ticks_step.1)
+                .map(|i| text![
+                    style! {St::FontSize => px(10)},
+                    attrs! {
+                        TextAnchor => "end",
+                        Fill => "#999",
+                        X => padding - 6,
+                        Y => i + padding + 4,
+                    },
+                    i,
+                ]),
+        ],
+        g![
+            class!["plot-area"],
+            polyline![attrs! {
+                Fill => "none",
+                Stroke => "rgb(0, 86, 91)",
+                Points => data
+                    .iter()
+                    .map(|(x, y)| format!(
+                        "{},{}",
+                        x / dx + padding as f64,
+                        -y / dx + (height/2) as f64,
+                    ))
+                    .collect::<Vec<_>>()
+                    .join(" ")
+            }],
+            data.iter()
+                .enumerate()
+                .filter_map(|(i, (x, y))| if i % 30 == 0 {
+                    let onenter = onenter.clone();
+                    let onout = onout.clone();
+                    Some(g![
+                        class!["data-point"],
+                        mouse_ev(Ev::MouseOver, move |event| {
+                            onenter(event.x(), event.y())
+                        }),
+                        mouse_ev(Ev::MouseOut, move |_| onout),
+                        circle![attrs! {
+                            Cx => x / dx + padding as f64,
+                            Cy => -y / dx + (height/2) as f64,
+                            R => 2,
+                            Fill => "rgba(0, 86, 91, 0.8)",
+                        }],
+                    ])
+                } else {
+                    None
+                })
+        ]
     ]
 }
