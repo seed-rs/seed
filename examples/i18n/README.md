783f9b7461660d62d25332fc72438af0b5d9f8c6
tokcum <tobias.mucke@gmail.com>
2020-05-28 19:01:21 +0200

    examples(i18n): Add example i18n leveraging fluent to implement multilingual support.

---
+++ examples/i18n/README.md
@@ -0,0 +1,19 @@
+## I18N example
+
+How to support multiple languages in your web app based on [Fluent][url_project_fluent]. 
+Includes a language selector, some sample text and [FTL strings][url_ftl_syntax_guide] 
+demonstrating the simplicity and power of [Seed][url_project_seed] 
+powered by [Fluent's Rust crate][url_crate_fluent].
+
+[url_project_fluent]: https://projectfluent.org/
+[url_crate_fluent]: https://docs.rs/fluent/
+[url_ftl_syntax_guide]: https://projectfluent.org/fluent/guide/
+[url_project_seed]: https://seed-rs.org/
+
+---
+
+```bash
+cargo make start
+```
+
+Open [127.0.0.1:8000](http://127.0.0.1:8000) in your browser.
---
+++ examples/i18n/src/i18n.rs
@@ -0,0 +1,76 @@
+use fluent::{FluentArgs, FluentBundle, FluentResource};
+use strum_macros::{AsRefStr, EnumIter, EnumString};
+use unic_langid::LanguageIdentifier;
+
+use super::resource::Resource;
+
+// ------ I18n ------
+
+pub struct I18n {
+    lang: Lang,
+    resource: FluentBundle<FluentResource>,
+}
+
+impl I18n {
+    pub fn new(lang: Lang) -> Self {
+        let mut i18n = Self {
+            lang,
+            resource: FluentBundle::default(),
+        };
+        i18n.set_lang(lang);
+        i18n
+    }
+
+    pub const fn lang(&self) -> &Lang {
+        &self.lang
+    }
+
+    pub fn set_lang(&mut self, lang: Lang) -> &Self {
+        self.lang = lang;
+        let ftl_res = FluentResource::try_new(
+            Resource::new()
+                .get(lang.as_ref())
+                .expect("get language identifier")
+                .parse()
+                .expect("parse language identifier"),
+        )
+        .expect("parse FTL string");
+
+        let locale: LanguageIdentifier = lang.as_ref().parse().expect("parse language identifier");
+
+        let mut bundle = FluentBundle::new(&[locale]);
+        bundle.add_resource(ftl_res).expect("add FTL resource");
+
+        self.resource = bundle;
+        self
+    }
+
+    pub fn translate(&self, key: &str, args: Option<&FluentArgs>) -> String {
+        let fluent_msg = self.resource.get_message(key).expect("get fluent message");
+        let pattern = fluent_msg.value.expect("get value for fluent message");
+
+        self.resource
+            .format_pattern(pattern, args, &mut vec![])
+            .to_string()
+    }
+}
+
+// ------ Lang ------
+
+#[allow(non_camel_case_types)]
+#[derive(Copy, Clone, EnumIter, EnumString, AsRefStr, PartialEq)]
+pub enum Lang {
+    #[strum(serialize = "en-US")]
+    EnUS,
+    #[strum(serialize = "de-DE")]
+    DeDE,
+}
+
+impl Lang {
+    pub fn label(&self) -> &str {
+        match self {
+            Self::EnUS => "English (US)",
+            Self::DeDE => "Deutsch (Deutschland)",
+        }
+    }
+}
