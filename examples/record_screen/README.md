24c773ec25d87ebf3c77abb2cd729b67f6344ea5
Martin Kavík <martin@kavik.cz>
2021-01-18 21:01:27 +0100

    examples: record_screen

---
+++ examples/record_screen/README.md
@@ -0,0 +1,11 @@
+## Record Screen example
+
+How to record the screen using the [Screen Capture API](https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture).
+
+---
+
+```bash
+cargo make start
+```
+
+Open [127.0.0.1:8000](http://127.0.0.1:8000) in your browser.
---
+++ examples/record_screen/src/lib.rs
@@ -0,0 +1,97 @@
+use seed::{prelude::*, *};
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{DisplayMediaStreamConstraints, HtmlMediaElement, MediaStream};
+
+// ------ ------
+//     Init
+// ------ ------
+
+fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
+    Model::default()
+}
+
+// ------ ------
+//     Model
+// ------ ------
+
+#[derive(Default)]
+struct Model {
+    video: ElRef<HtmlMediaElement>,
+}
+
+// ------ ------
+//    Update
+// ------ ------
+
+enum Msg {
+    RecordScreen,
+    DisplayMedia(Result<MediaStream, JsValue>),
+}
+
+fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::RecordScreen => {
+            orders.perform_cmd(display_media());
+        }
+        Msg::DisplayMedia(Ok(media_stream)) => {
+            model
+                .video
+                .get()
+                .expect("get video element")
+                .set_src_object(Some(&media_stream));
+        }
+        Msg::DisplayMedia(Err(error)) => {
+            log!(error);
+        }
+    }
+}
+
+async fn display_media() -> Msg {
+    let mut constraints = DisplayMediaStreamConstraints::new();
+    constraints.video(&JsValue::from(true));
+
+    let media_stream_promise = window()
+        .navigator()
+        .media_devices()
+        .unwrap()
+        .get_display_media_with_constraints(&constraints)
+        .unwrap();
+
+    Msg::DisplayMedia(
+        JsFuture::from(media_stream_promise)
+            .await
+            .map(MediaStream::from),
+    )
+}
+
+// ------ ------
+//     View
+// ------ ------
+
+fn view(model: &Model) -> Node<Msg> {
+    div![
+        button![
+            style! {
+                St::Display => "block",
+            },
+            "Record Screen",
+            ev(Ev::Click, |_| Msg::RecordScreen)
+        ],
+        video![
+            el_ref(&model.video),
+            attrs! {
+                At::Width => 1024,
+                At::AutoPlay => AtValue::None,
+            }
+        ]
+    ]
+}
+
+// ------ ------
+//     Start
+// ------ ------
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    App::start("app", init, update, view);
+}
